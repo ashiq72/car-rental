7479e9dd17d4ccb4a28b2a4c115d0508c3e4be49
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-08-27 13:39:50 +0600

    Product cads added

--- components/Home/CarsList.tsx
+++ components/Home/CarsList.tsx
@@ -2,7 +2,7 @@ import CarCard from "./CarCard";
 
 export default function CarsList(props: any) {
   return (
-    <div>
+    <div className="grid">
       {props.carsList.map((car: any, index: number) => (
         <CarCard key={index} car={car} />
       ))}
--- services/index.tsx
+++ services/index.tsx
@@ -13,6 +13,7 @@ export const getCarsList = async () => {
         publishedAt
         image {
           id
+          url
         }
       }
     }
